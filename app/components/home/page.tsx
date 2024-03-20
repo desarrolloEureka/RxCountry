1e6dc6b90c0a529017f764d22b62932b2f114a51
rodacapera <rodacapera@gmail.com>
2024-03-20 17:19:03 -0500

    bugFix: metadata fix

--- app/components/home/page.tsx
+++ app/components/home/page.tsx
@@ -1,14 +1,5 @@
 import Home from '@/components/home/Home';
 
-import { ResolvingMetadata } from 'next';
-
-export function generateMetadata(parent: ResolvingMetadata) {
-  return {
-    title: 'Home',
-    description: 'Dashboard in home page',
-  };
-}
-
 const page = () => {
   return <Home />;
 };
