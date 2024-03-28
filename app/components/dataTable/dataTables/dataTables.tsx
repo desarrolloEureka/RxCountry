2aac2a48af1d99b57db80fbf7645cf215fc33ced
diefer09 <fernando@eurekadreams.com>
2024-03-27 19:35:43 -0500

    fix: color added to logo and table title

--- app/components/dataTable/dataTables/dataTables.tsx
+++ app/components/dataTable/dataTables/dataTables.tsx
@@ -1,127 +1,159 @@
-import React, { MouseEvent } from 'react';
-import DataTable from 'react-data-table-component';
+import React, { MouseEvent } from "react";
+import DataTable, { createTheme } from "react-data-table-component";
 // import DataTableExtensions from 'react-data-table-component-extensions';
-const DataTableExtensions: any = dynamic(
-  () => import('react-data-table-component-extensions'),
-  { ssr: false }
+
+createTheme(
+    "solarized",
+    {
+        text: {
+            primary: "#268bd2",
+            secondary: "#2aa198",
+        },
+        background: {
+            default: "#002b36",
+        },
+        context: {
+            background: "#cb4b16",
+            text: "#FFFFFF",
+        },
+        divider: {
+            default: "#073642",
+        },
+        action: {
+            button: "rgba(0,0,0,.54)",
+            hover: "rgba(0,0,0,.08)",
+            disabled: "rgba(0,0,0,.12)",
+        },
+    },
+    "dark",
 );
-import { Button } from 'react-bootstrap';
-import 'react-data-table-component-extensions/dist/index.css';
-import dynamic from 'next/dynamic';
+
+const DataTableExtensions: any = dynamic(
+    () => import("react-data-table-component-extensions"),
+    { ssr: false },
+);
+import { Button } from "react-bootstrap";
+import "react-data-table-component-extensions/dist/index.css";
+import dynamic from "next/dynamic";
 import {
-  ExportProps,
-  UploadDataButtonModalProps,
-  UploadDataModalProps,
-} from '@/types/tables';
+    ExportProps,
+    UploadDataButtonModalProps,
+    UploadDataModalProps,
+} from "@/types/tables";
 
 function convertArrayOfObjectsToCSV(array: object[]): string {
-  let result: string;
+    let result: string;
 
-  const columnDelimiter = ',';
-  const lineDelimiter = '\n';
-  const keys = Object.keys(array[0]);
+    const columnDelimiter = ",";
+    const lineDelimiter = "\n";
+    const keys = Object.keys(array[0]);
 
-  result = '';
-  result += keys.join(columnDelimiter);
-  result += lineDelimiter;
-
-  array.forEach((item: any) => {
-    let ctr = 0;
-    keys.forEach((key: string) => {
-      if (ctr > 0) result += columnDelimiter;
-
-      result += item[key];
-
-      ctr++;
-    });
+    result = "";
+    result += keys.join(columnDelimiter);
     result += lineDelimiter;
-  });
 
-  return result;
+    array.forEach((item: any) => {
+        let ctr = 0;
+        keys.forEach((key: string) => {
+            if (ctr > 0) result += columnDelimiter;
+
+            result += item[key];
+
+            ctr++;
+        });
+        result += lineDelimiter;
+    });
+
+    return result;
 }
 
 function downloadCSV(array: any[]) {
-  const link = document.createElement('a');
-  let csv = convertArrayOfObjectsToCSV(array);
-  if (csv == null) return;
+    const link = document.createElement("a");
+    let csv = convertArrayOfObjectsToCSV(array);
+    if (csv == null) return;
 
-  const filename = 'export.csv';
+    const filename = "export.csv";
 
-  if (!csv.match(/^data:text\/csv/i)) {
-    csv = `data:text/csv;charset=utf-8,${csv}`;
-  }
+    if (!csv.match(/^data:text\/csv/i)) {
+        csv = `data:text/csv;charset=utf-8,${csv}`;
+    }
 
-  link.setAttribute('href', encodeURI(csv));
-  link.setAttribute('download', filename);
-  link.click();
+    link.setAttribute("href", encodeURI(csv));
+    link.setAttribute("download", filename);
+    link.click();
 }
 
 const Export = ({ onExport }: ExportProps) => (
-  <Button
-    onClick={(e: MouseEvent<HTMLButtonElement>) =>
-      onExport(e.currentTarget.value)
-    }
-  >
-    Exportar
-  </Button>
+    <Button
+        onClick={(e: MouseEvent<HTMLButtonElement>) =>
+            onExport(e.currentTarget.value)
+        }
+    >
+        Exportar
+    </Button>
 );
 
 const UploadDataCsvModal = ({
-  onUploadDataModalCsv,
+    onUploadDataModalCsv,
 }: UploadDataButtonModalProps) => (
-  <Button onClick={onUploadDataModalCsv}>Subir Csv</Button>
+    <Button onClick={onUploadDataModalCsv}>Subir Csv</Button>
 );
 
 const SalesModal = ({ onSalesModal }: UploadDataButtonModalProps) => (
-  <Button onClick={onSalesModal}>Vender</Button>
+    <Button onClick={onSalesModal}>Vender</Button>
 );
 
 const UploadDataPdfModal = ({
-  onUploadDataModalPdf,
+    onUploadDataModalPdf,
 }: UploadDataButtonModalProps) => (
-  <Button onClick={onUploadDataModalPdf}>Subir multiples Pdf</Button>
+    <Button onClick={onUploadDataModalPdf}>Subir multiples Pdf</Button>
 );
 
 export const ExportCSV = ({
-  onUploadDataModalCsv,
-  onUploadDataModalPdf,
-  onSalesModal,
-  data,
-  tableData,
-  columns,
-  noHeader = false,
-  tableTitle,
+    onUploadDataModalCsv,
+    onUploadDataModalPdf,
+    onSalesModal,
+    data,
+    tableData,
+    columns,
+    noHeader = false,
+    tableTitle,
 }: UploadDataModalProps) => {
-  const actionsMemo = React.useMemo(() => {
+    const actionsMemo = React.useMemo(() => {
+        return (
+            <>
+                <UploadDataCsvModal
+                    onUploadDataModalCsv={onUploadDataModalCsv}
+                />
+                {onUploadDataModalPdf && (
+                    <UploadDataPdfModal
+                        onUploadDataModalPdf={onUploadDataModalPdf}
+                    />
+                )}
+                {onSalesModal && <SalesModal onSalesModal={onSalesModal} />}
+                <Export onExport={() => downloadCSV(data)} />
+            </>
+        );
+    }, [data, onSalesModal, onUploadDataModalCsv, onUploadDataModalPdf]);
+
+    // return (
+    //   <DataTable columns={columns} data={data} actions={actionsMemo} pagination />
+    // );
+
     return (
-      <>
-        <UploadDataCsvModal onUploadDataModalCsv={onUploadDataModalCsv} />
-        {onUploadDataModalPdf && (
-          <UploadDataPdfModal onUploadDataModalPdf={onUploadDataModalPdf} />
-        )}
-        {onSalesModal && <SalesModal onSalesModal={onSalesModal} />}
-        <Export onExport={() => downloadCSV(data)} />
-      </>
+        <DataTableExtensions {...tableData} filterPlaceholder="Buscar">
+            <DataTable
+                // noHeader={noHeader}
+                defaultSortFieldId={2}
+                columns={columns}
+                data={data}
+                actions={actionsMemo}
+                pagination
+                highlightOnHover
+                title={tableTitle}
+                progressPending={data ? false : true}
+                theme="solarized"
+            />
+        </DataTableExtensions>
     );
-  }, [data, onSalesModal, onUploadDataModalCsv, onUploadDataModalPdf]);
-
-  // return (
-  //   <DataTable columns={columns} data={data} actions={actionsMemo} pagination />
-  // );
-
-  return (
-    <DataTableExtensions {...tableData} filterPlaceholder='Buscar'>
-      <DataTable
-        noHeader={noHeader}
-        defaultSortFieldId={2}
-        columns={columns}
-        data={data}
-        actions={actionsMemo}
-        pagination
-        highlightOnHover
-        title={tableTitle}
-        progressPending={data ? false : true}
-      />
-    </DataTableExtensions>
-  );
 };
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,82 +1,82 @@
-import { Tab } from 'react-bootstrap';
-import BannerMenu from '../bannerMenu/BannerMenu';
-import HomeDash from '../dashBoard/homeDash/HomeDash';
-import DataTableComponent from '../dataTable/DataTableComponent';
-import HeaderContent from './components/headerContent/HeaderContent';
+import { Tab } from "react-bootstrap";
+import BannerMenu from "../bannerMenu/BannerMenu";
+import HomeDash from "../dashBoard/homeDash/HomeDash";
+import DataTableComponent from "../dataTable/DataTableComponent";
+import HeaderContent from "./components/headerContent/HeaderContent";
 
 const Header = ({ hamburger }: { hamburger?: boolean }) => {
-  return (
-    <Tab.Container defaultActiveKey='first'>
-      <HeaderContent />
-      <Tab.Content>
-        <Tab.Pane
-          className='tab-pane show  text-muted'
-          id='home1'
-          role='tabpanel'
-          eventKey='first'
-        >
-          <HomeDash />
-        </Tab.Pane>
-        <Tab.Pane
-          className='tab-pane text-muted'
-          id='about1'
-          role='tabpanel'
-          eventKey='second'
-        >
-          <BannerMenu
-            seoTitle='Usuarios'
-            title='Usiarios'
-            item='Dashboard'
-            activeItem='Registro de usuarios'
-          />
-          <DataTableComponent
-            componentTitle='Tabla del Listado de usuarios.'
-            componentDescription='En esta tabla se encuentran listados todos los usuarios para su administración.'
-            tableTitle='Usuarios registrados'
-            reference='users'
-          />
-        </Tab.Pane>
-        <Tab.Pane
-          className='tab-pane text-muted'
-          id='service1'
-          role='tabpanel'
-          eventKey='third'
-        >
-          <BannerMenu
-            seoTitle='Clientes'
-            title='Clientes'
-            item='Dashboard'
-            activeItem='Registro clientes'
-          />
-          <DataTableComponent
-            componentTitle='Tabla del Listado de clientes.'
-            componentDescription='En esta tabla se encuentran listados todos los clientes para su administración.'
-            tableTitle='Clientes registrados'
-            reference='clients'
-          />
-        </Tab.Pane>
-        <Tab.Pane
-          className='tab-pane text-muted'
-          id='license1'
-          role='tabpanel'
-          eventKey='fourth'
-        >
-          <BannerMenu
-            seoTitle='Medicos'
-            title='Medicos'
-            item='Dashboard'
-            activeItem='Registro medicos'
-          />
-          <DataTableComponent
-            componentTitle='Tabla del Listado de medicos.'
-            componentDescription='En esta tabla se encuentran listados todos los medicos para su administración.'
-            tableTitle='Medicos registrados'
-            reference='doctors'
-          />
-        </Tab.Pane>
-      </Tab.Content>
-    </Tab.Container>
-  );
+    return (
+        <Tab.Container defaultActiveKey="first">
+            <HeaderContent />
+            <Tab.Content>
+                <Tab.Pane
+                    className="tab-pane show  text-muted"
+                    id="home1"
+                    role="tabpanel"
+                    eventKey="first"
+                >
+                    <HomeDash />
+                </Tab.Pane>
+                <Tab.Pane
+                    className="tab-pane text-muted"
+                    id="about1"
+                    role="tabpanel"
+                    eventKey="second"
+                >
+                    <BannerMenu
+                        seoTitle="Usuarios"
+                        title="Usuarios"
+                        item="Dashboard"
+                        activeItem="Registro de usuarios"
+                    />
+                    <DataTableComponent
+                        componentTitle="Tabla del Listado de usuarios."
+                        componentDescription="En esta tabla se encuentran listados todos los usuarios para su administración."
+                        tableTitle="Usuarios registrados"
+                        reference="users"
+                    />
+                </Tab.Pane>
+                <Tab.Pane
+                    className="tab-pane text-muted"
+                    id="service1"
+                    role="tabpanel"
+                    eventKey="third"
+                >
+                    <BannerMenu
+                        seoTitle="Pacientes"
+                        title="Pacientes"
+                        item="Dashboard"
+                        activeItem="Registro Pacientes"
+                    />
+                    <DataTableComponent
+                        componentTitle="Tabla del Listado de Pacientes."
+                        componentDescription="En esta tabla se encuentran listados todos los clientes para su administración."
+                        tableTitle=""
+                        reference="clients"
+                    />
+                </Tab.Pane>
+                <Tab.Pane
+                    className="tab-pane text-muted"
+                    id="license1"
+                    role="tabpanel"
+                    eventKey="fourth"
+                >
+                    <BannerMenu
+                        seoTitle="Medicos"
+                        title="Medicos"
+                        item="Dashboard"
+                        activeItem="Registro medicos"
+                    />
+                    <DataTableComponent
+                        componentTitle="Tabla del Listado de medicos."
+                        componentDescription="En esta tabla se encuentran listados todos los medicos para su administración."
+                        tableTitle="Medicos registrados"
+                        reference="doctors"
+                    />
+                </Tab.Pane>
+            </Tab.Content>
+        </Tab.Container>
+    );
 };
 
 export default Header;
